b2a386d964d71c1f64f54785f343f0359aea25fa
OguzhanKirik <kirikoguzhan@gmail.com>
2022-09-15 12:10:57 +0200

    ekf

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/oguz/ekf/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/oguz/ekf/catkin_ws/devel;/home/oguz/Desktop/catkin_ws/devel;/home/oguz/ros_workspace/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/oguz/ekf/catkin_ws/devel;/home/oguz/Desktop/catkin_ws/devel;/home/oguz/ros_workspace/catkin_ws/devel;/opt/ros/noetic' != '' else []
